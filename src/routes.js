4a3652a6a568922da534bf1ab3b9c8e502d23097
PeuStone <pedrohmotaaa@gmail.com>
2023-08-14 10:49:11 -0300

    Adicionando a pagina sobre

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import Menu from 'components/Menu';
 import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
+import Sobre from 'pages/Sobre';
 import Inicio from 'pages/inicio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -13,6 +14,7 @@ export default function AppRouter() {
           <Route path='/' element={<PaginaPadrao />}>
             <Route index element={<Inicio />} />
             <Route path='cardapio' element={<Cardapio />} />
+            <Route path='sobre' element={<Sobre />} />
           </Route>
         </Routes>
       </Router>
