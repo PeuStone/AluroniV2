a4330769b31143de2a019b127a72afd407a102cc
PeuStone <pedrohmotaaa@gmail.com>
2023-08-17 10:15:59 -0300

    Redirecionamento do cardapio pro item clicável

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,12 +1,14 @@
 import TagsPratos from 'components/TagsPrato';
 import styles from './Item.module.scss';
 import { Prato } from 'types/Prato';
+import { useNavigate } from 'react-router-dom';
 
 export default function Item(props: Prato) {
-  const { title, description,photo } = props;
+  const { title, description, photo, id } = props;
+  const navigate = useNavigate();
   return (
     <>
-      <div className={styles.item}>
+      <div className={styles.item} onClick={() => navigate(`/prato/${id}`)}>
         <div className={styles.item__imagem}>
           <img src={photo} alt={title} />
         </div>
