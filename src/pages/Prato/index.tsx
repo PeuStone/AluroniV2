679327c3695152c9a93010078780363fe68e9b28
PeuStone <pedrohmotaaa@gmail.com>
2023-08-17 10:39:43 -0300

    Acabando com alguns problemas do header

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -3,34 +3,37 @@ import { useParams, useNavigate } from 'react-router-dom';
 import cardapio from 'data/cardapio.json';
 import TagsPratos from 'components/TagsPrato';
 import NotFound from 'pages/NotFound';
+import PaginaPadrao from 'components/PaginaPadrao';
 
 export default function Prato() {
   const { id } = useParams();
   const navigate = useNavigate();
   const prato = cardapio.find(item => item.id === Number(id));
-  if(!prato) {
+  if (!prato) {
     return <NotFound />;
   }
- 
+
   return (
     <>
-      <button className={styles.voltar} onClick={() => navigate(-1)}>
-        {'< Voltar'}
-      </button>
-      <section className={styles.container}>
-        <h1 className={styles.titulo}>
-          {prato.title}
-        </h1>
-        <div className={styles.imagem}>
-          <img src={prato.photo} alt={prato.title} />
-        </div>
-        <div className={styles.conteudo}>
-          <p className={styles.conteudo__descricao}>
-            {prato.description}
-          </p>
-          <TagsPratos {...prato} />
-        </div>
-      </section>
+      <PaginaPadrao>
+        <button className={styles.voltar} onClick={() => navigate(-1)}>
+          {'< Voltar'}
+        </button>
+        <section className={styles.container}>
+          <h1 className={styles.titulo}>
+            {prato.title}
+          </h1>
+          <div className={styles.imagem}>
+            <img src={prato.photo} alt={prato.title} />
+          </div>
+          <div className={styles.conteudo}>
+            <p className={styles.conteudo__descricao}>
+              {prato.description}
+            </p>
+            <TagsPratos {...prato} />
+          </div>
+        </section>
+      </PaginaPadrao>
     </>
   );
 }
